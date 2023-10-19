fdeda91a52a7e3658d281f20ce15590c72c25cc6
DISTREAT <99132213+DISTREAT@users.noreply.github.com>
2023-10-19 16:21:12 +0200

    build: add benchmarking

---
+++ benchmark.ts
@@ -0,0 +1,17 @@
+import { Suite } from "asyncmark";
+import { Generator } from "./src/index";
+import { PassThrough } from "node:stream";
+
+const generator = new Generator();
+const suite = new Suite({ parallel: true });
+
+suite.add({
+    number: 1000,
+    fun () {
+        const xs = new PassThrough();
+        generator.generateImage(xs, 200, 100, "random");
+    }
+});
+
+suite.run();
+
