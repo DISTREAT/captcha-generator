a1b51f4bfae74224b7a28b066051b85e6dc13f81
DISTREAT <99132213+DISTREAT@users.noreply.github.com>
2023-10-20 04:01:50 +0200

    feat: do not rely on gm dependency

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import gmConstructor from "gm";
 import { Writable } from "node:stream";
-
-const gm = gmConstructor.subClass({ imageMagick: true });
+import spawn from "cross-spawn";
 
 export interface GeneratorOptions {
+    /// path to imagemagick executable
+    executable?: string,
     /// width of the captcha
     width?: number,
     /// height of the captcha
@@ -21,6 +21,7 @@ export interface GeneratorOptions {
 }
 
 const defaultGeneratorOptions: GeneratorOptions = {
+    executable: "/usr/bin/convert",
     width: 200,
     height: 85,
     padding: 26,
@@ -32,14 +33,34 @@ const defaultGeneratorOptions: GeneratorOptions = {
 
 export class Generator {
     private options: GeneratorOptions;
+    private openingArgumentsIM: string[];
+    private closingArgumentsIM: string[];
 
     constructor(options: GeneratorOptions) {
         this.options = { ...defaultGeneratorOptions, ...options };
+        this.openingArgumentsIM = [
+            "-size",
+            `${this.options.width!}x${this.options.height!}`,
+            "xc:#ffffff",
+            "-pointsize",
+            `${this.options.fontSize!}`
+        ];
+        this.closingArgumentsIM = [
+            "-swirl",
+            `${this.options.swirl!}`,
+            "+noise",
+            "impulse",
+            "-compress",
+            "LZMA",
+            "-quality",
+            "60",
+            "webp:-"
+        ];
     }
 
     generateImage(writeStream: Writable, solution: string): void {
-        const context = gm(this.options.width!, this.options.height!, "#ffffff");
         let last_position_x = this.options.padding! - this.options.spacing!;
+        const drawingArgumentsIM: string[] = [];
 
         for (let index = 0; index < solution.length; index++) {
             last_position_x = getRandomInt(
@@ -50,24 +71,39 @@ export class Generator {
                 )
             ),
 
-            context.fontSize(this.options.fontSize!).draw(`
-                translate
+            drawingArgumentsIM.push(
+                "-draw",
+                `translate
                 ${last_position_x},${getRandomInt(this.options.padding!, this.options.height! - this.options.padding!)}
                 rotate
                 ${getRandomInt(this.options.rotationLimit![0], this.options.rotationLimit![1])}
                 text
                 0,0
                 "${solution[index]}"
-            `);
+                `
+            );
         }
 
-        context.swirl(this.options.swirl!);
-        context.noise("impulse");
+        const proc = spawn(
+            this.options.executable!,
+            [
+                this.openingArgumentsIM,
+                drawingArgumentsIM,
+                this.closingArgumentsIM
+            ].flat()
+        );
 
-        context.compress("LZMA").quality(60).stream("jpeg", function (error, stdout, stderr) {
-            if (error) throw error;
-            stdout.pipe(writeStream);
+        proc.on("close", function (code, signal) {
+           if (code !== 0 || signal !== null) {
+               throw new Error("There was an issue with ImageMagick");
+           };
         });
+
+        if (proc.stdout) {
+            proc.stdout!.pipe(writeStream);
+        } else {
+            throw new Error("No image to stream");
+        }
     }
 }
 
